227e1184808838fa75c86af5075f1039346e086e
Magnus Watn <magnus@watn.no>
2018-08-12 23:04:07 +0200

    s/BUSINESS/ENTERPRISE

--- api/sertifikatsok/constants.py
+++ api/sertifikatsok/constants.py
@@ -15,7 +15,7 @@ PERSONAL_SERIAL_REGEX = re.compile(r"9578-(4505|4050|4510)-[A-z0-9]+")
 
 class CertType(Enum):
     PERSONAL = 1
-    BUSINESS = 2
+    ENTERPRISE = 2
     UNKNOWN = 3
 
 SUBJECT_FIELDS = {
@@ -48,30 +48,30 @@ KEY_USAGES = [
 # and a string with an human friendly description
 KNOWN_CERT_TYPES = {
     ("C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 Test4 CA 3", "2.16.578.1.26.1.0.3.2"):
-    (CertType.BUSINESS, "Buypass TEST virksomhetssertifikat (softsertifikat)"),
+    (CertType.ENTERPRISE, "Buypass TEST virksomhetssertifikat (softsertifikat)"),
 
     # Have no source for this, just a guess based on the prod oid
     ("C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 Test4 CA 3", "2.16.578.1.26.1.0.3.5"):
-    (CertType.BUSINESS, "Buypass TEST virksomhetssertifikat (smartkort)"),
+    (CertType.ENTERPRISE, "Buypass TEST virksomhetssertifikat (smartkort)"),
 
     ("C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 CA 3", "2.16.578.1.26.1.3.2"):
-    (CertType.BUSINESS, "Buypass virksomhetssertifikat (softsertifikat)"),
+    (CertType.ENTERPRISE, "Buypass virksomhetssertifikat (softsertifikat)"),
 
     ("C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 CA 3", "2.16.578.1.26.1.3.5"):
-    (CertType.BUSINESS, "Buypass virksomhetssertifikat (smartkort)"),
+    (CertType.ENTERPRISE, "Buypass virksomhetssertifikat (smartkort)"),
 
     ("C=NO, O=Buypass, CN=Buypass Class 3 Test4 CA 1", "2.16.578.1.26.1.0.3.2"):
-    (CertType.BUSINESS, "Buypass TEST virksomhetssertifikat (softsertifikat)"),
+    (CertType.ENTERPRISE, "Buypass TEST virksomhetssertifikat (softsertifikat)"),
 
     # Have no source for this, just a guess based on the prod oid
     ("C=NO, O=Buypass, CN=Buypass Class 3 Test4 CA 1", "2.16.578.1.26.1.0.3.5"):
-    (CertType.BUSINESS, "Buypass TEST virksomhetssertifikat (smartkort)"),
+    (CertType.ENTERPRISE, "Buypass TEST virksomhetssertifikat (smartkort)"),
 
     ("C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 CA 1", "2.16.578.1.26.1.3.2"):
-    (CertType.BUSINESS, "Buypass virksomhetssertifikat (softsertifikat)"),
+    (CertType.ENTERPRISE, "Buypass virksomhetssertifikat (softsertifikat)"),
 
     ("C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 CA 1", "2.16.578.1.26.1.3.5"):
-    (CertType.BUSINESS, "Buypass virksomhetssertifikat (smartkort)"),
+    (CertType.ENTERPRISE, "Buypass virksomhetssertifikat (smartkort)"),
 
     ("C=NO, O=Buypass,  CN=Buypass Class 3 Test4 CA 1", "2.16.578.1.26.1.0",):
     (CertType.PERSONAL, "Buypass TEST person-sertifikat (smartkort)"),
@@ -89,13 +89,13 @@ KNOWN_CERT_TYPES = {
     (CertType.PERSONAL, "Buypass person-sertifikat (HSM)"),
 
     ("CN=CPN Enterprise SHA256 CLASS 3, OU=Commfides Trust Environment (c) 2011 Commfides Norge AS, O=Commfides Norge AS - 988 312 495, C=NO", "2.16.578.1.29.13.1.1.0"):
-    (CertType.BUSINESS, "Commfides virksomhetssertifikat"),
+    (CertType.ENTERPRISE, "Commfides virksomhetssertifikat"),
 
     ("CN=Commfides CPN Enterprise-Norwegian SHA256 CA - TEST, OU=Commfides Trust Environment(C) 2014 Commfides Norge AS - TEST, OU=CPN Enterprise-Norwegian SHA256 CA- TEST, O=Commfides Norge AS - 988 312 495, C=NO", "2.16.578.1.29.913.1.1.0"):
-    (CertType.BUSINESS, "Commfides TEST virksomhetssertifikat"),
+    (CertType.ENTERPRISE, "Commfides TEST virksomhetssertifikat"),
 
     ("CN=Commfides CPN Enterprise-Norwegian SHA256 CA - TEST2, OU=Commfides Trust Environment(C) 2014 Commfides Norge AS - TEST, OU=CPN Enterprise-Norwegian SHA256 CA- TEST2, O=Commfides Norge AS - 988 312 495, C=NO", "2.16.578.1.29.913.1.1.0"):
-    (CertType.BUSINESS, "Commfides TEST virksomhetssertifikat"),
+    (CertType.ENTERPRISE, "Commfides TEST virksomhetssertifikat"),
 
     ("CN=CPN Person High SHA256 CLASS 3, OU=Commfides Trust Environment (c) 2011 Commfides Norge AS, O=Commfides Norge AS - 988 312 495, C=NO", "2.16.578.1.29.12.1.1.0"):
     (CertType.PERSONAL, "Commfides person-sertifikat"),
